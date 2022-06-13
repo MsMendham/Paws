883c8239a423ab1d05e4a3c3d65627e44f434b6d
MsMendham <bmwilliamson4@gmail.com>
2022-06-13 13:23:04 +0100

    added volunteer and css gets

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ endpoint.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"../client/index.html"));
 });
 
+endpoint.get('/volunteer', (req, res) => {
+  res.sendFile(path.join(__dirname,"../client/form.html"));
+});
+
+endpoint.get('/css/*', (req, res) => {
+  res.sendFile(path.join(__dirname,"../client/",req.path));
+});
+
 endpoint.listen(port, () => {
   console.log(`running on port ${port}`);
 });
