fe1f5dc8d4a0989259b8ecb2e24f9898bd960e20
MsMendham <bmwilliamson4@gmail.com>
2022-06-13 12:48:15 +0100

    server file change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const endpoint = express();
 const port = 80;
 
 endpoint.get('/', (req, res) => {
-  res.send('noice');
+  res.sendFile(path.join(__dirname,"../client/index.html"));
 });
 
 endpoint.listen(port, () => {
