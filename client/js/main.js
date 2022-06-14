1398e4a0112fc3215de4a70efbf7e90d286a7429
lewardo <lewardo23@gmail.com>
2022-06-14 18:02:51 +0100

    added other components

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,8 @@
 new window.Vue({
   el: '#main',
   components: {
-    'rspca-header': window.httpVueLoader('/components/header.vue')
+    'rspca-header': window.httpVueLoader('/components/header.vue'),
+    'rspca-footer': window.httpVueLoader('/components/footer.vue'),
+    'rspca-content': window.httpVueLoader('/components/content.vue'),
   },
 })
