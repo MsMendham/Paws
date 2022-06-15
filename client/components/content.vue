d3f58a5fc59647511d29a2c8f31de9d301ff5951
lewardo <lewardo23@gmail.com>
2022-06-15 08:52:02 +0100

    content title banner

--- client/components/content.vue
+++ client/components/content.vue
@@ -1,16 +1,65 @@
 <template>
   <section id="content">
-    <slot></slot>
+    <div id="banner">
+      <span id="title">{{ $attrs.title }}</span>
+      <span id="subtitle">{{ $attrs.subtitle }}</span>
+    </div>
+    <div id="innercontent">
+      <slot></slot>
+    </div>
   </section>
 </template>
+
+<script>
+  module.exports = {
+    mounted: () => {
+      if ($attrs.title == undefined)
+        document.getElementById("banner").remove();
+    }
+  }
+</script>
 
 <style scoped>
 @import '/css/global.css';
 @import '/css/animation.css';
 
-section#content {
+#content {
   width: 100vw;
   min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: green;
+}
+
+#banner {
+  width: 100%;
+  background: #718ab1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#banner * {
+  color: white;
+}
+
+#title {
+  font-size: 48px;
+  font-weight: bold;
+  padding: 20px 0 0;
+}
+
+#subtitle {
+  font-size: 8px;
+  font-weight: thin;
+  padding: 20px 0;
+}
+
+#innercontent {
+  width: 55%;
+  min-height: 50vh;
+  background: red;
 }
 
 </style>
