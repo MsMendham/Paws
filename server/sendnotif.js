ae9907b34f04a986dd199ea0368f42627e5e9355
samsternofusa <stillmorgan05@gmail.com>
2022-06-16 13:38:52 +0100

    Added database querier and tidied up code

--- server/sendnotif.js
+++ server/sendnotif.js
@@ -5,10 +5,13 @@ const { send } = require('process')
 module.exports = {
     send: () => {
         console.log('Hy')
-        selections = database.checkDatabase()
-        for (let i = 0; i < selections.length; i++) {
-            console.log(selections[i])
-            // emailer.emailSender()
-        }
+        selections = database.checkDatabase(function(result){
+            selections = result;
+            console.log(selections)
+            for (let i = 0; i < result.length; i++) {
+                console.log(i + selections[i] + '\n\n')
+                emailer.emailSender(selections[i][0] + ' ' + selections[i][1], selections[i][3], selections[i][4], selections[i][5])
+            } 
+        })        
     },
 }
