87f380aa818aa979f4fbdf16e2796603aa2fb9c6
lewardo <lewardo23@gmail.com>
2022-06-16 17:10:51 +0100

    cardgrid initial

--- client/components/cardgrid.vue
+++ client/components/cardgrid.vue
@@ -1,11 +1,24 @@
 <template>
-  
+  <div id="section">
+    <section id="popup">
+
+    </section>
+    <section id="cardgrid">
+
+    </section>
+  </div>
 </template>
 
 <script>
-
+  module.exports = {
+    data: async () => {
+      return {
+        roles: JSON.parse(await fetch("/static/volunteering.json").then(res => res.text())).data
+      }
+    }
+  }
 </script>
 
-<style scoped>
+<style>
 
 </style>
